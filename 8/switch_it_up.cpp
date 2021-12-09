60d800f666a060c109b55dcbc048a735eaa27d8a
Arseniy Titov <70908119+famaxth@users.noreply.github.com>
2021-12-09 17:23:27 +0300

    Create switch_it_up.cpp

---
+++ 8/switch_it_up.cpp
@@ -0,0 +1,39 @@
+#include <string>
+
+std::string switch_it_up(int na){
+  int n = na;
+  switch (n)
+  {
+      case 0:
+        return "Zero";
+        break;
+      case 1:
+        return "One";
+        break;
+      case 3:
+        return "Three";
+        break;
+      case 4:
+        return "Four";
+        break;
+      case 5:
+        return "Five";
+        break;
+      case 6:
+        return "Six";
+        break;
+      case 7:
+        return "Seven";
+        break;
+      case 8:
+        return "Eight";
+        break;
+      case 9:
+        return "Nine";
+        break;
+      default:
+        return "fsd";
+        break;
+  }
+}
+  
