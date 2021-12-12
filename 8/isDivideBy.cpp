06d0cae0ba2df2c497e3f16fc18079b78c6b25a7
Arseniy Titov <70908119+famaxth@users.noreply.github.com>
2021-12-12 18:20:17 +0300

    Update isDivideBy.cpp

--- 8/isDivideBy.cpp
+++ 8/isDivideBy.cpp
@@ -1,4 +1,1 @@
-bool isDivideBy(int number, int a, int b)
-{
-    return ((number % a == 0) & (number % b == 0)) ? true : false;
-}
+bool isDivideBy(int number, int a, int b) {return ((number % a == 0) & (number % b == 0)) ? true : false;}
