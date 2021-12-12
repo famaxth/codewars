0a31e97f0122c8d12403dcac8fe45b9e0150edaf
Arseniy Titov <70908119+famaxth@users.noreply.github.com>
2021-12-12 18:27:28 +0300

    Update rental_car_cost.cpp

--- 8/rental_car_cost.cpp
+++ 8/rental_car_cost.cpp
@@ -1,6 +1,1 @@
-int rental_car_cost(int d)
-{
-
-  return d >= 7 ? 40 * d - 50 : d >= 3 ? 40 * d - 20 : 40 * d;
-  
-}
+int rental_car_cost(int d) {return d >= 7 ? 40 * d - 50 : d >= 3 ? 40 * d - 20 : 40 * d;}
