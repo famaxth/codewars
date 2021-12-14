64a779dc7a16fb4d5717daaedcc2283ee5116046
Arseniy Titov <70908119+famaxth@users.noreply.github.com>
2021-12-14 22:34:00 +0300

    Update number_to_string.cpp

--- 8/number_to_string.cpp
+++ 8/number_to_string.cpp
@@ -1,8 +1,5 @@
 #include <string>
-#include <sstream>
 
 std::string number_to_string(int num) {
-    std::ostringstream oss;
-    oss << num;
-    return oss.str();
+  return std::to_string(num);
 }
