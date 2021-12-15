45dd2b12ce7f0b0d7b5a92a1a2d9a08cf4d2e439
Arseniy Titov <70908119+famaxth@users.noreply.github.com>
2021-12-15 21:56:09 +0300

    Update arrayPlusArray.cpp

--- 8/arrayPlusArray.cpp
+++ 8/arrayPlusArray.cpp
@@ -1,1 +1,8 @@
+​#include <vector>
 
+int arrayPlusArray(std::vector<int> a, std::vector<int> b) {
+    int sum = 0;
+    for (int i = 0; i < a.size(); i++) {sum += a[i];}
+    for (int j = 0; j < b.size(); j++) {sum += b[j];}
+    return sum;
+}
